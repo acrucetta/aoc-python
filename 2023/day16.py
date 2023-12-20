981a8d70e50c8598cd04c5a7c70f4f9f397d02d9
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-12-19 22:45:27 -0600

    day 16: set-up day 16 for dfs traversal with queue

---
+++ 2023/day16.py
@@ -0,0 +1,206 @@
+from enum import Enum
+import sys
+import os
+from typing import Tuple
+
+import numpy as np
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)
+
+import common.util as util
+
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+"""
+Day 16: The Floor Will be Lava
+
+We receive a grid containing empty space (.) mirrors (/ and \)
+and splitters (| and -). 
+
+Each tile converts some of the beam's light into heat to melt the
+rock in the cavern.
+
+Beam behavior:
+- Empty space = continue same direction
+- Mirror (/ or \) = change direction 90 degrees depending on the angle
+    - / -> upward if coming from left, downward if coming from right
+    - \ -> upward if coming from right, downward if coming from left
+- Splitter (| or -)
+    - - -> continue straight if coming from left or right
+    - | -> continue straight if coming from up or down
+    - - -> split into two beams if coming from up or down
+    - | -> split into two beams if coming from left or right
+
+Beam caveats:
+- Beams do not interact with each other
+- A tile is energized if that tile has at least one beam on it
+
+.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....
+
+We want a count of the number of energized tiles. We assume
+the beam starts in the top-left heading right.
+
+Approach:
+- We need to keep track of the energized tiles; we can use a set
+- We want to traverse a graph given specific rules for each tile
+    Based on each rule we will pick a direction to go
+    To pick the direction we need to know the tile type; 
+    and the direction the beam is coming from. With these two
+    we can determine the direction to go next.
+- We will need to manage multiple beams at once; that is, 
+    we can have many iterations of the beam.
+    
+Data Structures:
+- We can use a 2D array to represent the grid
+- We can use a set to represent the energized tiles
+
+Traversal:
+- We can use a queue to manage the beams and the directions we need to go
+  i.e., we can have two valid directions when we add a value to the queue
+- We will implement a BFS traversal
+"""
+
+DAY = 16
+MAIN_PATH = f"2023/data/day{DAY:02d}.txt"
+SAMPLE_PATH = f"2023/data/day{DAY:02d}-sample.txt"
+
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    # Make the point subscriptable
+    def __getitem__(self, index: int) -> int:
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Invalid index")
+
+    def __repr__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+
+class Direction(Enum):
+    UP = (0, -1)
+    DOWN = (0, 1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+
+    def __repr__(self) -> str:
+        return f"<{self.value[0]}, {self.value[1]}>"
+
+    # Make the direction subscriptable
+    def __getitem__(self, index: int) -> int:
+        if index in [0, 1]:
+            return self.value[index]
+        else:
+            raise IndexError("Invalid index")
+
+
+def handle_empty(direction: Direction) -> Direction:
+    return direction
+
+
+def handle_mirror_slash(direction: Direction) -> list[Direction]:
+    return [Direction.UP if direction == Direction.LEFT else Direction.DOWN]
+
+
+def handle_mirror_backslash(direction: Direction) -> list[Direction]:
+    return [Direction.UP if direction == Direction.RIGHT else Direction.DOWN]
+
+
+def handle_splitter_horizontal(direction: Direction) -> list[Direction]:
+    return (
+        [direction]
+        if direction in [Direction.LEFT, Direction.RIGHT]
+        else [Direction.UP, Direction.DOWN]
+    )
+
+
+def handle_splitter_vertical(direction: Direction) -> list[Direction]:
+    return (
+        [direction]
+        if direction in [Direction.UP, Direction.DOWN]
+        else [Direction.LEFT, Direction.RIGHT]
+    )
+
+
+tile_actions = {
+    ".": handle_empty,
+    "/": handle_mirror_slash,
+    "\\": handle_mirror_backslash,
+    "-": handle_splitter_horizontal,
+    "|": handle_splitter_vertical,
+}
+
+
+def part1(input: list[list[str]]) -> int:
+    grid = util.Grid(input)
+    dfs(grid)
+    return 0
+
+
+def dfs(
+    grid: util.Grid,
+    start: Tuple[Tuple[int, int], Direction] = ((0, 0), Direction.RIGHT),
+) -> int:
+    """
+    Traverse the grid using DFS.
+    """
+    # Start at the top-left (0, 0)
+    queue = [start]
+    energized = set()
+    while queue:
+        point, direction = queue.pop(0)
+        energized.add((point, direction))
+
+        # Get the next point in the direction
+        next_point = grid.move_coordinates(point, direction)
+
+        tile = grid[next_point]
+
+        # Get the next directions to go
+        next_directions = tile_actions[tile](direction)
+        for next_direction in next_directions:
+            queue.append((next_point, next_direction))
+
+    return len(energized)
+
+
+def part2(input: list[list[str]]) -> int:
+    pass
+    return 0
+
+
+if __name__ == "__main__":
+    util.set_debug(False)
+    input: list[list[str]] = util.read_str_grid(MAIN_PATH)
+    sample: list[list[str]] = util.read_str_grid(SAMPLE_PATH)
+
+    print("PART 1")
+    part1(sample)
+    # part1(input)
+
+    # print("PART 2")
+    # part2(input)
+    # part2(sample)
--- common/util.py
+++ common/util.py
@@ -4,16 +4,20 @@ Utility functions and classes for Advent of Code
 
 import math
 import copy
+from typing import Tuple
 import numpy as np
+import shapely.geometry
+import shapely.affinity
 
-# References: 
+# References:
 # github.com/borja
 
 #
 # DEBUGGING/LOGGING
 #
 
-DEBUG=False
+DEBUG = False
+
 
 def set_debug(debug):
     """
@@ -27,11 +31,12 @@ def log(*args):
     """
     Prints a debugging message (if debugging messages are enabled)
     """
-    if DEBUG: 
-        print('\x1b[7;30;47m', end="")
+    if DEBUG:
+        print("\x1b[7;30;47m", end="")
         print(*args, end="")
-        print('\x1b[0m')
-        
+        print("\x1b[0m")
+
+
 def call_and_print(fn, *args):
     """
     Call a function with some parameters, and print the
@@ -49,6 +54,7 @@ def call_and_print(fn, *args):
 # FILE I/O
 #
 
+
 def read_strs(filename, sep=None):
     """
     Read strings from a file, separated by whitespace or by the
@@ -56,42 +62,45 @@ def read_strs(filename, sep=None):
     """
     with open(filename) as f:
         txt = f.read().strip()
-        strs = txt.split(sep=sep)    
+        strs = txt.split(sep=sep)
     return strs
 
+
 def read_grid(filepath):
-    '''
+    """
     Reads matrix from the filepath.
-    '''
+    """
     with open(filepath) as f:
         data = f.readlines()
-    
-    rows = list(map(lambda x: x.strip('\n'),data))
+
+    rows = list(map(lambda x: x.strip("\n"), data))
     matrix = []
-    
+
     for row in rows:
         split_row = [int(r) for r in row]
         matrix.append(split_row)
-    
+
     return np.array(matrix)
 
+
 def read_str_grid(filepath):
-    '''
+    """
     Reads matrix from the filepath.
-    '''
+    """
     with open(filepath) as f:
         data = f.readlines()
-    
-    rows = list(map(lambda x: x.strip('\n'),data))
+
+    rows = list(map(lambda x: x.strip("\n"), data))
     matrix = []
-    
+
     for row in rows:
         split_row = [r for r in row]
         matrix.append(split_row)
-    
-    return np.array(matrix)
 
-def parse_grid(strs : str) -> list[list[str]]:
+    return matrix
+
+
+def parse_grid(strs: str) -> list[list[str]]:
     """
     Parse a string into a grid.
     """
@@ -99,94 +108,165 @@ def parse_grid(strs : str) -> list[list[str]]:
     grid = [list(row) for row in rows]
     return grid
 
+
+#
+# DIRECTIONS
+#
+
+
+class Direction:
+    def __init__(self, x, y):
+        self._p = shapely.geometry.Point(x, y)
+
+    @classmethod
+    def UP(cls):
+        return cls(0, -1)
+
+    @classmethod
+    def DOWN(cls):
+        return cls(0, 1)
+
+    @classmethod
+    def LEFT(cls):
+        return cls(-1, 0)
+
+    @classmethod
+    def RIGHT(cls):
+        return cls(1, 0)
+
+    def __eq__(self, other):
+        # return self._p.x == other._p.x and self._p.y == other._p.y
+        return self._p == other._p
+
+    def __hash__(self):
+        # return hash((self._p.x, self._p.y))
+        return hash(self._p)
+
+    def __copy__(self):
+        return Direction(self._p.x, self._p.y)
+
+    def __repr__(self) -> str:
+        return f"<{self._p.x}, {self._p.y}>"
+
+
 #
 # GRID CLASS
 #
 
+
 class Grid:
-    '''
+    """
     A class used to read a grid and manipulate its positions
     (x,y) where the top-left position is (0,0)
-    '''
-    # Useful for problems that require checking adjacent positions (from Borja's Grid class)
-    DIRECTIONS = [(-1, -1), (0, -1), (+1,-1),
-                  (-1,  0),          (+1, 0),
-                  (-1, +1), (0, +1), (+1,+1)]
+    """
 
-    CARDINAL_DIRS = [          (0, -1),
-                     (-1,  0),          (+1, 0),
-                               (0, +1)         ]   
-    
-    
-    def __init__(self,grid):
+    # Useful for problems that require checking adjacent positions (from Borja's Grid class)
+    DIRECTIONS = [
+        (-1, -1),
+        (0, -1),
+        (+1, -1),
+        (-1, 0),
+        (+1, 0),
+        (-1, +1),
+        (0, +1),
+        (+1, +1),
+    ]
+
+    CARDINAL_DIRS = [(0, -1), (-1, 0), (+1, 0), (0, +1)]
+
+    def __init__(self, grid):
         self._grid = copy.deepcopy(grid)
-        self.max_x = len(grid[0])
-        self.max_y = len(grid)
+
+    @property
+    def max_x(self):
+        return len(self._grid[0])
     
-    def __validate_coordinates(self,x,y):
+    @property
+    def max_y(self):
+        return len(self._grid)
+    
+    def move_coordinates(self, coord, direction) -> Tuple[int, int]:
+        """
+        Move a coordinate in the specified direction.
+        """
+        next_x = coord[0] + direction[0]
+        next_y = coord[1] + direction[1]
+        self.__validate_coordinates(coord[0], coord[1])
+        self.__validate_coordinates(next_x, next_y)
+        return next_x, next_y
+        
+
+    def __validate_coordinates(self, x, y):
         orig_x = x
         orig_y = y
         if orig_x < 0 or orig_x >= self.max_x:
             raise IndexError("Invalid x coordinate: {}".format(orig_x))
         if orig_y < 0 or orig_y >= self.max_y:
             raise IndexError("Invalid y coordinate: {}".format(orig_y))
-        return x,y
-    
+        return x, y
+
     def __str__(self):
-        '''
+        """
         Prints the existing grid class.
-        '''
+        """
         rows = [", ".join(str(x) for x in row) for row in self._grid]
         return "\n".join(rows)
-    
-    def __getitem__(self,pos):
-        '''
+
+    def __getitem__(self, pos):
+        """
         Returns the value at the specified position.
-        '''
-        x,y = self.__validate_coordinates(pos[0],pos[1])
-        
+        """
+        x, y = self.__validate_coordinates(pos[0], pos[1])
+
         return self._grid[y][x]
-    
-    def __setitem__(self,pos,value):
-        '''
+
+    def __setitem__(self, pos, value):
+        """
         Sets the value at the specified position.
-        '''
-        x,y = self.__validate_coordinates(pos[0],pos[1])
+        """
+        x, y = self.__validate_coordinates(pos[0], pos[1])
         self._grid[y][x] = value
-    
+
     def __iter__(self):
-        '''
+        """
         Iterates over the grid.
-        '''
+        """
         for row in self._grid:
             for item in row:
                 yield item
-    
-    def get_adjacent_positions(self,pos):
-        '''
+
+    def get_adjacent_positions(self, pos):
+        """
         Returns a list of adjacent positions to the specified position.
-        '''
-        x, y = self.__validate_coordinates(pos[0],pos[1])
-        positions = {"UP":(x,y-1),"DOWN":(x,y+1),"RIGHT":(x+1,y),"LEFT":(x-1,y),
-                     "UP-LEFT":(x-1,y-1),"UP-RIGHT":(x+1,y-1),"DOWN-LEFT":(x-1,y+1),"DOWN-RIGHT":(x+1,y+1)}
-        for adj,pos in positions.items():
+        """
+        x, y = self.__validate_coordinates(pos[0], pos[1])
+        positions = {
+            "UP": (x, y - 1),
+            "DOWN": (x, y + 1),
+            "RIGHT": (x + 1, y),
+            "LEFT": (x - 1, y),
+            "UP-LEFT": (x - 1, y - 1),
+            "UP-RIGHT": (x + 1, y - 1),
+            "DOWN-LEFT": (x - 1, y + 1),
+            "DOWN-RIGHT": (x + 1, y + 1),
+        }
+        for adj, pos in positions.items():
             try:
-                x, y = self.__validate_coordinates(pos[0],pos[1])
+                x, y = self.__validate_coordinates(pos[0], pos[1])
             except IndexError:
                 positions[adj] = None
         return positions
-    
-    def get_cardinal_positions(self,pos):
-        '''
+
+    def get_cardinal_positions(self, pos):
+        """
         Returns a list of cardinal positions to the specified position.
-        '''
-        x,y = self.__validate_coordinates(pos[0],pos[1])
-        positions = {"N":(x,y-1),"S":(x,y+1),"E":(x+1,y),"W":(x-1,y)}
+        """
+        x, y = self.__validate_coordinates(pos[0], pos[1])
+        positions = {"N": (x, y - 1), "S": (x, y + 1), "E": (x + 1, y), "W": (x - 1, y)}
         # Check that all cardinal positions are valid
-        for card,pos in positions.items():
+        for card, pos in positions.items():
             try:
-                x, y = self.__validate_coordinates(pos[0],pos[1])
+                x, y = self.__validate_coordinates(pos[0], pos[1])
             except IndexError:
                 positions[card] = None
         return positions
-        

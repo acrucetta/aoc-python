2a6dc20f9b6355adf37c1418e5eabecbe13afe9f
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-12-16 09:43:12 -0600

    feat(day15): implement grid widening and robot movement logic for Advent of Code challenge
    Adds widen_grid method to expand map tiles, updates move_robot and _move_objects methods to handle more complex movement scenarios, and enhances part1 and part2 functions with improved debugging and grid manipulation

--- 2024/day15.py
+++ 2024/day15.py
@@ -63,36 +63,51 @@ class Grid:
                 if val == "@":
                     self.robot = (row, col)
 
+    def widen_grid(self):
+        """
+        To get the wider warehouse's map, start with your original map and,
+        for each tile, make the following changes:
+        - If the tile is #, the new map contains ## instead.
+        - If the tile is O, the new map contains [] instead.
+        - If the tile is ., the new map contains .. instead.
+        - If the tile is @, the new map contains @. instead.
+        This will produce a new warehouse map which is twice as wide and with wid
+        """
+        for row, row_vals in enumerate(self.grid):
+            for col, val in enumerate(row_vals):
+                if val == "#":
+                    self.grid[row][col] = "##"
+                elif val == "O":
+                    self.grid[row][col] = "[]"
+                elif val == ".":
+                    self.grid[row][col] = ".."
+                elif val == "@":
+                    self.grid[row][col] = "@."
+
     def in_bounds(self, point: Tuple[int, int]):
         row, col = point
         if 0 <= row <= self.max_row and 0 <= col <= self.max_col:
             return True
         return False
 
-
     def _move_objects(self, obj: Tuple[int, int], direction: Tuple[int, int]):
-        """
-        Move object and anything ahead of it.
-        For the next cell, move it to the one ahead
-        of it, until we hit a wall.
-
-        If the object has only walls ahead of it, remain
-        it in place.
-
-        If it can move ahead, move the object ahead and
-        anything ahead of it.
-        """
-        dr, dc = direction.to_tuple()  # type: ignore
+        dr, dc = direction
         r, c = obj
         nr, nc = r + dr, c + dc
         if not self.in_bounds((nr, nc)):
             return
 
         next_cell = self.grid[nr][nc]
-        if next_cell == ".":
+        if next_cell == "#":
+            # We can't move the object past the current cell
+            return
+        elif next_cell == ".":
+            # We can move the object with no impediments
             self.grid[nr][nc] = "O"
             self.grid[r][c] = "."
         elif next_cell == "O":
+            # We have an object on the way, move that
+            # object too
             self._move_objects((nr, nc), direction)
             if self.grid[nr][nc] == ".":
                 self.grid[nr][nc] = "O"
@@ -107,7 +122,7 @@ class Grid:
         ahead of it, until we hit the wall.
         Case 3: Moving into a wall, remain in place.
         """
-        dr, dc = direction.to_tuple()  # type: ignore
+        dr, dc = direction.to_tuple()
         r, c = self.robot
         nr, nc = r + dr, c + dc
         if self.in_bounds((nr, nc)):
@@ -115,26 +130,38 @@ class Grid:
             if next_cell == ".":
                 # Move to the next location
                 self.robot = (nr, nc)
+                self.grid[r][c] = "."
+                self.grid[nr][nc] = "@"
             elif next_cell == "#":
                 # Remain in place
                 self.robot = (r, c)
             elif next_cell == "O":
-                self._move_objects((nr, nc))
-                pass
-    
+                self._move_objects((nr, nc), (dr, dc))
+
     def __str__(self):
         grid_str = "\n".join("".join(row) for row in self.grid)
         return f"{grid_str}"
 
 
 def part1(input: str) -> int:
-    grid = util.parse_grid(input[0])
+    grid = Grid(util.parse_grid(input[0]))
     moves = parse_moves(input[1])
-    print(grid)
+    print("Initial state:\n\n", grid)
+    for i, move in enumerate(moves[:5]):
+        print(f"Move #: {i}; {move.to_tuple()}")
+        grid.move_robot(move)
+        print(grid)
 
 
 def part2(input: str) -> int:
-    pass
+    grid = Grid(util.parse_grid(input[0]))
+    moves = parse_moves(input[1])
+    grid.widen_grid()
+    print("Initial state:\n\n", grid)
+    for i, move in enumerate(moves[:5]):
+        print(f"Move #: {i}; {move.to_tuple()}")
+        grid.move_robot(move)
+        print(grid)
 
 
 if __name__ == "__main__":
@@ -142,11 +169,9 @@ if __name__ == "__main__":
     input = util.read_strs(MAIN_PATH, "\n\n")
     sample = util.read_strs(SAMPLE_PATH, "\n\n")
 
-    parse_moves("<vv>^<v^>")
-
     print("PART 1")
     # util.call_and_print(part1, input)
-    util.call_and_print(part1, sample)
+    # util.call_and_print(part1, sample)
 
     print("PART 2")
     # part2(input)
